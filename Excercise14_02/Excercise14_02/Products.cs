468a4e253686eabe096ccfe34ca2765c19fc117a
long10a10302 <89914013+long10a10302@users.noreply.github.com>
2025-02-19 15:56:33 +0700

    baitapvenha14-02

---
+++ Excercise14_02/Excercise14_02/Products.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise14_02
+{
+    internal class Products
+    {
+        private static int Count = 0;
+        public string ID { get; private set; }
+        public string Name { get; private set; }
+        public int Quantity {  get; private set; }
+
+        public Products()
+        {
+            Count++;
+            ID = "Hang" + Count.ToString();
+            Name = "No name";
+            Quantity = 0;
+        }
+
+        public void ShowDetail()
+        {
+            Console.WriteLine("{0,-10} {1,-20} {2,-10}", ID, Name, Quantity);
+        }
+
+        public void SetDetail(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+
+        public void AddQuantity(int addQuantity)
+        {
+            Quantity += addQuantity;
+        }
+    }
+}
---
+++ Excercise14_02/Excercise14_02/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise14_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Products product = new Products();
+
+            Console.Write("Nhập tên hàng: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Nhập số lượng: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            product.SetDetail(name, quantity);
+            product.ShowDetail();
+
+            Console.Write("Nhập số lượng muốn thêm: ");
+            int moreQuantity = int.Parse(Console.ReadLine());
+
+            product.AddQuantity(moreQuantity);
+            product.ShowDetail();
+        }
+    }
+}
